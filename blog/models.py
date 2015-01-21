23680ca3fec05eafea75a0bf627b8c47bf7fcb13
Bansal Utkarsh <bansalutkarsh3@gmail.com>
2015-01-22 02:58:49 +0530

    Adds the Entry model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class EntryQuerySet(models.QuerySet):
+    def published(self):
+        return self.filter(publish=True)
+
+class Entry(models.Model):
+    title = models.CharField(max_length=200)
+    body = models.TextField()
+    slug = models.SlugField(max_length=200, unique=True)
+    publish = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    object = EntryQuerySet.as_manager()
+
+    def __unicode__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Blog Entry'
+        verbose_name_plural = 'Blog Entries'
+        ordering = ['-created']
