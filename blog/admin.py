74ca9132fd04bef756fc686ab81b458efcdd9237
Bansal Utkarsh <bansalutkarsh3@gmail.com>
2015-01-22 03:26:28 +0530

    Integrates django-summernote(wysiwyg) with admin
    * Adds django-summernote editor to the admin form
    * Update requirements file

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from .models import Entry
+from django_summernote.admin import SummernoteModelAdmin
 
 
-class EntryAdmin(admin.ModelAdmin):
+class EntryAdmin(SummernoteModelAdmin):
     list_display = ('title', 'created')
     prepopulated_fields = {'slug':('title',)}
 
